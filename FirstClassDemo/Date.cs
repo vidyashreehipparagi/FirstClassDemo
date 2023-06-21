a88e3e613e8f9d6c7c39d9dca83499fffe285ff2
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-06-21 16:18:14 +0530

    Add project files.

---
+++ FirstClassDemo/Date.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClassDemo
+{
+    public class Date
+    {
+        private int day, year;
+        private string month;
+
+        public void AcceptData(int dd,string mm,int yr)
+        {
+            day = dd;
+            month = mm;
+            year = yr;
+        }
+        public string PrintDate()
+        {
+            return $"Date is {day}-{month}-{year}";
+        }
+        public int PrintYear()
+        {
+            return year ;
+        }
+    }
+}
---
+++ FirstClassDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClassDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date date1= new Date();
+            date1.AcceptData(16, "june", 2023);
+            string data=date1.PrintDate();
+            Console.WriteLine(data);
+            Console.WriteLine(date1.PrintDate());
+            int y=date1.PrintYear();
+            Console.WriteLine(y);
+           // Console.WriteLine(date1.GetMonth();
+        }
+    }
+}
